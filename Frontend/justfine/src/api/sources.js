50e2288abf1de06891a30d719b165331b8ac79b6
Monisha V <monishav643@gmail.com>
2026-07-31 22:27:39 +0530

    Change request body to empty JSON object
    Update the request body to send an empty JSON object instead of sourceData.

--- Frontend/justfine/src/api/sources.js
+++ Frontend/justfine/src/api/sources.js
@@ -31,7 +31,7 @@ const addSource = async () =>{
                 "Content-Type": "application/json",
                 Authorization: `Bearer ${token}`
             },
-            body: JSON.stringify(sourceData),
+            body: JSON.stringify(),
         });
         const data = await response.json();
 
